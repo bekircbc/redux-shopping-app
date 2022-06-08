cdc97971e4a29e4b5e13cdaa32dfafbb3f2c71e1
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-08 10:06:20 +0200

    main.jsx structutre changed..

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import ReactDOM from "react-dom/client";
 import App from "./app/App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
