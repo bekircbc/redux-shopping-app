5d6df67cc9af6ea9ad6687ac24ae558e066d8dfd
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-08 09:48:52 +0200

    main.jsx changed..

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app/App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-)
+  </Provider>,
+  document.getElementById("root")
+);
